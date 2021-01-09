479626074f6a22590f25949c239633c1d1925dec
KurtHermansen <kurtis.hermansen@gmail.com>
2021-01-09 12:52:02 -0700

    year commit

--- js/aboutMe.js
+++ js/aboutMe.js
@@ -1,2 +1,4 @@
 const date = document.lastModified;
- document.getElementById("lastMod").innerHTML = date;
+    document.getElementById("lastMod").innerHTML = date;
+const year = new Date().getFullYear();  
+    document.getElementById("currentYear").innerHTML = year;
