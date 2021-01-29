ff10522d147aa15b26c2eda41cb13ee57b86781c
KurtHermansen <kurtis.hermansen@gmail.com>
2021-01-29 12:31:20 -0700

    preston commit

---
+++ Lesson4/js/weather.js
@@ -0,0 +1,13 @@
+
+const date = new Date();
+    var weekDay = new Array("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday");
+    var monthName = new Array("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December");
+    var day = date.getDay();
+    var dayN = date.getDate();
+    var month = date.getMonth();
+    var year = date.getUTCFullYear();
+    document.getElementById("currentDate").innerHTML = weekDay[day] +", " + dayN + " " + monthName[month]+ " " + year;
+    document.getElementById("currentYear").innerHTML = year;
+function toggleMenu(){
+    document.getElementsByClassName("menu")[0].classList.toggle("burger");
+}
