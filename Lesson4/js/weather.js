d91e1cfea3a6e2dd5fd86884547777115d6fca6c
KurtHermansen <kurtis.hermansen@gmail.com>
2021-01-30 20:44:15 -0700

    img commit

--- Lesson4/js/weather.js
+++ Lesson4/js/weather.js
@@ -1,6 +1,6 @@
 
 const date = new Date();
-    var weekDay = new Array("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday");
+    var weekDay = new Array("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday");
     var monthName = new Array("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December");
     var day = date.getDay();
     var dayN = date.getDate();
